97d000b788c72da8d7d558baa03ae33a56f2ae1a
Jaroslaw Kostujak <jaroslaw.kostujak.programista@gmail.com>
2024-09-12 22:02:06 +0200

    comparing values and object using assertsions

---
+++ src/test/java/org/example/MealTest.java
@@ -0,0 +1,57 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MealTest {
+
+    @Test
+    void shouldReturnDiscountedPrice() {
+
+        //given
+        Meal meal = new Meal(35);
+
+        //when
+        int discounted = meal.getDiscountedPrice(7);
+
+        //then
+        assertEquals(28, discounted);
+
+    }
+
+    @Test
+    void referencesToTheSameObjectShouldBeEqual() {
+
+        //given
+        Meal meal1 = new Meal(10);
+        Meal meal2 = meal1;
+
+        //then
+        assertSame(meal1, meal2);
+
+    }
+
+    @Test
+    void referencesToDifferentObjectsShouldNotBeEqual() {
+
+        //given
+        Meal meal1 = new Meal(10);
+        Meal meal2 = new Meal(20);
+
+        //then
+        assertNotSame(meal1, meal2);
+
+    }
+
+    @Test
+    void twoMealsShouldBeEqualWhenPriceAndNameAreTheSame() {
+
+        //given
+        Meal meal1 = new Meal(10, "Pizza");
+        Meal meal2 = new Meal(10, "Pizza");
+
+        //then
+        assertEquals(meal1, meal2);
+    }
+}
