88f4b6c7a670cf6edf48ab86890c73df4fe597d2
Jaroslaw Kostujak <jaroslaw.kostujak.programista@gmail.com>
2024-09-13 22:52:02 +0200

    Adnotation @Disabled and @DisplayName

--- src/test/java/org/example/CartTest.java
+++ src/test/java/org/example/CartTest.java
@@ -1,14 +1,18 @@
 package org.example;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Test cases for Cart")
 class CartTest {
 
     @Test
+    @DisplayName("Cart is able to process 1000 orders in 100ms")
     void simulateLargeOrder() {
 
         //given
@@ -17,6 +21,18 @@ class CartTest {
         //when
         //then
         assertTimeout(Duration.ofMillis(100), cart::simulateLargeOrder);
+    }
+
+    @Disabled
+    @Test
+    void simulateLargeOrder2() {
+
+        //given
+        Cart cart = new Cart();
+
+        //when
+        //then
+        assertTimeout(Duration.ofMillis(1), cart::simulateLargeOrder);
     }
 
 }
