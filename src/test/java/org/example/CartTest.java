3ea57ce0d135b7a54b82e86dd75b132ecc91a7aa
Jaroslaw Kostujak <jaroslaw.kostujak.programista@gmail.com>
2024-09-18 22:25:36 +0200

    Matcher for join assertions

--- src/test/java/org/example/CartTest.java
+++ src/test/java/org/example/CartTest.java
@@ -5,7 +5,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
+import java.util.List;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Test cases for Cart")
@@ -35,4 +38,41 @@ class CartTest {
         assertTimeout(Duration.ofMillis(1), cart::simulateLargeOrder);
     }
 
+    @Test
+    void cartShouldBeNotEmptyAfterAddingOrderToCart() {
+
+        //given
+        Order order = new Order();
+        Cart cart = new Cart();
+
+        //when
+        cart.addOrderToCart(order);
+
+        //then
+        assertThat(cart.getOrders(), anyOf(
+                notNullValue(),
+                hasSize(1),
+                is(not(empty())),
+                is(not(emptyCollectionOf(Order.class)))
+        ));
+
+        assertThat(cart.getOrders(), allOf(
+                notNullValue(),
+                hasSize(1),
+                is(not(empty())),
+                is(not(emptyCollectionOf(Order.class)))
+        ));
+
+        assertAll(
+                () -> assertThat(cart.getOrders(), notNullValue()),
+                () -> assertThat(cart.getOrders(), hasSize(1)),
+                () -> assertThat(cart.getOrders(), is(not(empty()))),
+                () -> assertThat(cart.getOrders(), is(not(emptyCollectionOf(Order.class)))),
+                () -> {
+                    List<Meal> mealList = cart.getOrders().get(0).getMeals();
+                    assertThat(mealList, empty());
+
+                }
+        );
+    }
 }
